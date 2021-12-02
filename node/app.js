b99881be993f049359745f444b2a410b3facde72
Joona <97.joona@gmail.com>
2021-12-02 13:58:32 +0200

    hls server ver1

---
+++ node/app.js
@@ -0,0 +1,58 @@
+const http = require('http');
+var static = require('node-static');
+var url = require('url');
+var fs = require('fs');
+
+var file = new(static.Server)(__dirname);
+var spawn = require('child_process').spawn;
+var cmd = 'ffmpeg';
+var args = [
+    "-analyzeduration", "300M",
+    "-probesize", "300M",
+    "-protocol_whitelist", "file,udp,rtp",
+    "-i", "../sdp/test.sdp",
+    "-profile:v", "baseline",
+    "-level", "3.0",
+    "-s", "640x360",
+    "-start_number", "0",
+    "-hls_time", "2",
+    "-hls_list_size", "10",
+    "-hls_delete_threshold", "10",
+    "-hls_flags", "delete_segments",
+    "-f", "hls", "hls/index.m3u8"
+]
+
+const hostname = '127.0.0.1';
+const port = 8070;
+
+//remove old streams, todo: remove old stream files after the stream has ended
+fs.rmSync("hls", { recursive: true, force: true});
+fs.mkdirSync("hls");
+
+function runFfmpeg() {
+    var proc = spawn(cmd, args);
+    proc.stdout.on('data', function(data) {
+        console.log(data);
+    });
+    proc.stderr.setEncoding("utf8")
+    proc.stderr.on('data', function(data) {
+        console.log(data);
+    });
+    proc.on('close', function() {
+        console.log('finished');
+    });
+  }
+
+http.createServer((req, res) => {
+    var q = url.parse(req.url, true);
+    var path = q.pathname
+    if (path.startsWith("/hls")) {
+        file.serve(req, res);
+    } else if (path == "/ffmpeg") {
+        runFfmpeg();
+    } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("hey");
+    }
+}).listen(port);
